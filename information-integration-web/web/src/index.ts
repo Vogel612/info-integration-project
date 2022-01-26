6b897e60de7c22e4daba822bf7064c317ed79377
Clemens Lieb <vogel612@gmx.de>
2022-01-26 16:00:48 +0100

    Add collapsible visualization container

--- information-integration-web/web/src/index.ts
+++ information-integration-web/web/src/index.ts
@@ -1,10 +1,14 @@
-import 'bootstrap';
+import * as bs from 'bootstrap';
 import './style/base.css';
 
 import api from './api';
 import bodyRender from './views/index.hbs';
 
 document.body.innerHTML = bodyRender({}, {});
+
+
+let collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+collapseElementList.map((el: any) => new bs.Collapse(el))
 
 api.allTitles()
     .then(titles => {
