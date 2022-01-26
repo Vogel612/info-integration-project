bb8df454202cc0d7e20c3ca166d3f3be8e3fe740
Clemens Lieb <vogel612@gmx.de>
2022-01-26 16:16:44 +0100

    Initial rendering for anime search results

--- information-integration-web/web/src/index.ts
+++ information-integration-web/web/src/index.ts
@@ -1,8 +1,11 @@
 import * as bs from 'bootstrap';
 import './style/base.css';
 
+import $ from 'jquery';
+
 import api from './api';
 import bodyRender from './views/index.hbs';
+import animeCard from './views/template/anime_title.hbs';
 
 document.body.innerHTML = bodyRender({}, {});
 
@@ -12,7 +15,9 @@ collapseElementList.map((el: any) => new bs.Collapse(el))
 
 api.allTitles()
     .then(titles => {
+        const resultContainer = $('#results');
+        resultContainer.children().remove();
         for (const t of titles) {
-            console.log(t['title']);
+            resultContainer.append(animeCard(t, {}));
         }
     });
