1e65596f5644ce22110da121a0b4465500df6b3a
Clemens Lieb <vogel612@gmx.de>
2022-01-27 00:10:54 +0100

    Use JQuery for rendering

--- information-integration-web/web/src/index.ts
+++ information-integration-web/web/src/index.ts
@@ -1,23 +1,28 @@
 import * as bs from 'bootstrap';
 import './style/base.css';
 
+import * as d3 from 'd3';
 import $ from 'jquery';
 
 import api from './api';
 import bodyRender from './views/index.hbs';
 import animeCard from './views/template/anime_title.hbs';
 
-document.body.innerHTML = bodyRender({}, {});
+function render() {
+    let content = $(bodyRender({}, {}));
+    $('body').children().remove();
+    $('body').append(content);
 
+    $('.collapse', content).each((_, el) => { new bs.Collapse(el) });
 
-let collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-collapseElementList.map((el: any) => new bs.Collapse(el))
+    api.allTitles()
+        .then(titles => {
+            const resultContainer = $('#results');
+            resultContainer.children().remove();
+            for (const t of titles) {
+                resultContainer.append(animeCard(t, {}));
+            }
+        });
+}
 
-api.allTitles()
-    .then(titles => {
-        const resultContainer = $('#results');
-        resultContainer.children().remove();
-        for (const t of titles) {
-            resultContainer.append(animeCard(t, {}));
-        }
-    });
+$(render)
